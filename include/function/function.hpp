38bfe346c1f96c0a470a7ee1bca5356ad901f81d
AnShuai666 <1028792866@qq.com>
2019-03-04 18:00:15 +0800

    添加区间取值判断宏

--- include/function/function.hpp
+++ include/function/function.hpp
@@ -59,6 +59,18 @@ gaussian_xx(T const& xx, T const& sigma2);
 template <typename T>
 T
 gaussian2_xx(T const& xx, T const& sigma2);
+
+/*
+*  @property   限定范围
+*  @func       限定v在min与max之间
+*  @param_in   v                 输入值
+*  @param_in   min               最小值
+*  @param_in   max               最大值
+*  @return     T
+*/
+template <typename T>
+T const&
+clamp(T const& v, T const& min, T const& max);
 FUNCTION_NAMESPACE_END
 
 /********************************************************************
@@ -90,6 +102,13 @@ inline T
 function::gaussian2_xx(const T &xx, const T &sigma2)
 {
     return std::exp(-(xx) / (T(2) * sigma2));
+}
+
+template <typename T>
+T const&
+clamp(T const& v, T const& min, T const& max)
+{
+    return (v < min ? min : (v > max ? max : v));
 }
 FUNCTION_NAMESPACE_END
 
