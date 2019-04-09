502af439d7ee6c56bc3fbe8c9579b2d0d84706de
AnShuai666 <1028792866@qq.com>
2019-04-09 09:21:22 +0800

    更新一些配置文件

---
+++ installMATH.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ "$#" -eq 0 ]
+then
+    echo "利用默认安装路径:/usr/local"
+    INSTALL_DIR="/usr/local"
+elif [ "#$" -eq 1 ]
+then
+    echo "利用用户自定义安装文件夹:$1"
+    INSTALL_DIR=$1
+else
+    echo "ERROR"
+fi
+
+rm -drf build
+rm -drf $INSTALL_DIR/include/MATH
+rm -drf $INSTALL_DIR/lib/MATH
+rm -drf $INSTALL_DIR/bin/MATH
+rm -drf $INSTALL_DIR/share/MATH
+
+
+
+mkdir build
+
+cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}
+make
+make install
