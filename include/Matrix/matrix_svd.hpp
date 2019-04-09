37d768175ce6c3373fb545b807e6e81e3abff53d
AnShuai666 <1028792866@qq.com>
2019-04-09 11:16:49 +0800

    添加几个矩阵分解的文件

---
+++ include/Matrix/matrix_svd.hpp
@@ -0,0 +1,21 @@
+/*
+ * 日期:	2018-04-09
+ * 时间:	10:58
+ * 姓名:	安帅
+ * @func    矩阵SVD分解
+*/
+
+#ifndef MATH_MATRIX_SVD_HPP
+#define MATH_MATRIX_SVD_HPP
+
+#include <math_define.h>
+#include "define.h"
+MATH_NAMESPACE_BEGIN
+MATRIX_NAMESPACE_BEGIN
+
+
+
+
+MATRIX_NAMESPACE_END
+MATH_NAMESPACE_END
+#endif //MATH_MATRIX_SVD_HPP
