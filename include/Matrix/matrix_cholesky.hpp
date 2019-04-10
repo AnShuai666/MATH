27efe75ea06c2231f7430298b2338bd040f5e1c6
AnShuai666 <1028792866@qq.com>
2019-04-10 10:56:29 +0800

    更新Cholesky

--- include/Matrix/matrix_cholesky.hpp
+++ include/Matrix/matrix_cholesky.hpp
@@ -13,6 +13,13 @@
 MATH_NAMESPACE_BEGIN
 MATRIX_NAMESPACE_BEGIN
 
+/*
+ *  @property   Cholesky分解
+ *  @func       将矩阵A进行Cholesky分解
+ *  @param_in   A       待分解矩阵A
+ *  @param_in   cols    矩阵列数
+ *  @param_in   L       A的下三角矩阵
+*/
 template <typename T>
 void
 cholesky_decomposition(T const* A,int const cols,T* L);
@@ -21,6 +28,37 @@ cholesky_decomposition(T const* A,int const cols,T* L);
 
 
 
+MATRIX_NAMESPACE_END
+MATH_NAMESPACE_END
+
+
+MATH_NAMESPACE_BEGIN
+MATRIX_NAMESPACE_BEGIN
+
+template<typename T>    
+void 
+math::matrix::cholesky_decomposition(T const *A, int const cols, T *L) 
+{
+    T* out_ptr = L;
+    //TODO:CUDA@杨丰拓
+    //计算下三角对角线左侧数据
+    for (int i = 0; i < ; ++i)
+    {
+        
+    }
+
+    //计算下三角对角线数据
+    for (int j = 0; j < ; ++j) {
+
+    }
+    //填充下三角右侧数据项为0
+
+    for (int k = 0; k < ; ++k) {
+
+    }
+
+}
+    
 MATRIX_NAMESPACE_END
 MATH_NAMESPACE_END
 #endif //MATH_CHOLESKY_HPP
