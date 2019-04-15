27be6d9e87633e486e5e940f07654f593f7ea583
Quinlan94 <xzky15@163.com>
2019-04-15 09:06:27 +0800

    QR分解函数未测试

--- include/Matrix/matrix_qr.hpp
+++ include/Matrix/matrix_qr.hpp
@@ -18,6 +18,8 @@
 
 #include <math_define.h>
 #include "define.h"
+#include "matrix.hpp"
+#include "vector.hpp"
 MATH_NAMESPACE_BEGIN
 MATRIX_NAMESPACE_BEGIN
 
@@ -32,9 +34,10 @@ MATRIX_NAMESPACE_BEGIN
  *  @param_in   episilon
 */
 template <typename T>
-void qr_decomposition(T const* A,int rows,int coll,T* Q,T* R,T const& epsilon = T(1e-12));
+void qr_decomposition(T const* A,int rows=3,int col=3,T* Q,T* R,T const& epsilon = T(1e-12));
 
-
+template <typename T,int m,int n>
+void qr_decomposition(Matrix<T,m,n> *A,Matrix<T,m,m>* Q,Matrix<T,m,n>* R,T const& epsilon = T(1e-12));
 MATRIX_NAMESPACE_END
 MATH_NAMESPACE_END
 
@@ -43,12 +46,39 @@ MATH_NAMESPACE_BEGIN
 MATRIX_NAMESPACE_BEGIN
 
 //TODO:@邹云龙 实现之过程重要步骤注释
+//TODO:没有实现动态模板，矩阵不能初始化，函数未能测试。
 template <typename T>
 void
-math::matrix::qr_decomposition(T const* A,int rows,int coll,T* Q,T* R,T const& epsilon )
+math::matrix::qr_decomposition(T const* A, int rows, int cols,T* Q,T* R,T const& epsilon )
 {
 
+    rows =(const int) 3;
+    Matrix<T,rows,cols> matrix_A(A),matrix_R;
+    Matrix<T,rows,rows> matrix_Q;
+    qr_decomposition(matrix_A,Q,R,epsilon);
 
+}
+template <typename T,int m,int n>
+void
+math::matrix::qr_decomposition(Matrix<T,m,n> *A,Matrix<T,m,m>* Q,Matrix<T,m,n>* R,T const& epsilon )
+{
+   //施密特方法
+    for (int i = 0; i < n; ++i)
+    {
+        Vector<T,m> x = A->col(i);
+        for (int j = 0; j < i; ++j)
+        {
+            x-=A->col(i).dot(Q->col(j))*Q->col(j)/Q->col(j)*Q->col(j);
+        }
+        Q->col(i)=x;
+    }
+  //正交化
+    for (int k = 0; k <n; ++k)
+    {
+        double dev=Q->col(k).dot(Q->col(k));
+        dev < epsilon ? Q->col(k)*=0:Q->col(k)/=sqrt(dev);
+    }
+    R = Q->transpose()*A;
 }
 
 MATRIX_NAMESPACE_END
