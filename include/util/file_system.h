0977103a2d41eb3cf3c8bf726e96bd50ba3601af
AnShuai666 <1028792866@qq.com>
2019-04-18 21:36:00 +0800

    添加文件系统实现函数

--- include/util/file_system.h
+++ include/util/file_system.h
@@ -2,7 +2,7 @@
  * 日期:	2018-12-1
  * 时间:	10:12
  * 姓名:	安帅
- * @func    矩阵类 矩阵常规运算
+ * @func    文件系统
 */
 
 #ifndef MATH_FILE_SYSTEM_H
@@ -12,10 +12,31 @@
 
 UTIL_NAMESPACE_BEGIN
 FILE_SYSTEM_NAMESPACE_BEGIN
-bool dir_exists(char const* pathname);
 
-bool file_exists(char const* pathname);
+/*
+ *  @property   文件夹检测函数
+ *  @func       检测文件夹是否存在
+ *  @param_in   pathname        路径名
+ *  @return     bool
+ *              存在             true
+ *              不存在           false   errno被设置
+*/
+bool is_directory_exist(char const* pathname);
+/*
+ *  @property   文件检测函数
+ *  @func       检测文件是否存在
+ *  @param_in   pathname        路径名
+ *  @return     bool
+ *              存在             true
+ *              不存在           false   errno被设置
+*/
+bool is_file_exists(char const* pathname);
 
+/*
+ *  @property   用户目录获取
+ *  @func       获取用户目录/home/user
+ *  @return     char const*     用户目录
+*/
 char const* get_home_dir(void);
 
 char* get_cwd(char* buf,std::size_t size);
--- src/util/file_system.cpp
+++ src/util/file_system.cpp
@@ -1,5 +1,98 @@
-//
-// Created by yhl on 19-4-17.
-//
+/*
+ * 日期:	2018-12-1
+ * 时间:	10:12
+ * 姓名:	安帅
+ * @func    文件系统
+*/
 
 #include "util/file_system.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <cerrno>
+#include <cstdio>
+#include <pwd.h>
+#include <cstring>
+#ifndef  PATH_MAX
+#define PATH_MAX 2048
+#endif
+
+UTIL_NAMESPACE_BEGIN
+
+char home_path[PATH_MAX] = { 0 };
+
+bool
+file_system::is_directory_exist(char const *pathname)
+{
+    struct stat statebuf;
+    if(::stat(pathname,&statebuf) < 0)
+    {
+        std::perror("stat");
+        return false;
+    }
+
+    if (!S_ISDIR(statebuf.st_mode))
+    {
+        std::perror("S_ISDIR");
+        return false;
+    }
+
+    return true;
+}
+
+bool
+file_system::is_file_exists(char const *pathname)
+{
+#ifdef  _WIN32
+    struct _stat statbuf;
+    if(::_stat(pathname,&statbuf) < 0)
+        {
+        return false;
+        }
+
+    if(!(statbuf.st_mode & _S_IFREG)
+    {
+        return false;
+    }
+#else
+    struct stat statbuf;
+    if(::stat(pathname,&statbuf) < 0)
+    {
+        std::perror("stat");
+        return false;
+    }
+
+    if (!S_ISREG(statbuf.st_mode))
+    {
+        std::perror("S_ISREG");
+        return false;
+    }
+
+    return true;
+#endif
+}
+
+
+char const*
+file_system::get_home_dir(void)
+{
+    if (*home_path != 0)
+    {
+        return home_path;
+    }
+
+    //geteuid()用来取得执行目前进程有效的用户识别码.
+    uid_t user_id = ::geteuid();
+    //getpwuid()用来逐一搜索参数uid 指定的用户识别码, 找到时便将该用户的数据以结构返回
+    struct passwd* user_info = ::getpwuid(user_id);
+    if (user_info == nullptr || user_info->pw_dir == nullptr)
+    {
+        //TODO:抛出异常
+    }
+    std::strncpy(home_path,user_info->pw_dir,PATH_MAX);
+
+    return home_path;
+}
+
+
+UTIL_NAMESPACE_END
