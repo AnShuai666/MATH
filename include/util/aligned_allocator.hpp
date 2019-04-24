d5a6418740c8d5eb9d34f17bebe3e06c68ca479c
AnShuai666 <1028792866@qq.com>
2019-04-24 17:00:02 +0800

    内存对齐

--- include/util/aligned_allocator.hpp
+++ include/util/aligned_allocator.hpp
@@ -17,6 +17,7 @@
 #endif
 UTIL_NAMESPACE_BEGIN
 
+
 template <typename T,size_t alignment>
 struct AlignedAllocator
 {
@@ -41,6 +42,14 @@ public:
     template <class T_other,size_t alignment_other>
     AlignedAllocator(AlignedAllocator<T_other,alignment_other> const&);
 
+    /*
+     *  @property   内存分配并对齐
+     *  @func       分配n*sizeof(T)个字节的内存给pointer,
+     *              且内存的地址是alignment的倍数
+     *              alignment必须是2的幂
+     *  @param_in   n           要分配T类型内存单元的大小
+     *  @return     pointer     返回分配内存的首地址
+    */
     pointer allocate(size_type n);
     void deallocate(pointer p, size_type n);
     size_type max_size(void) const;
@@ -53,6 +62,61 @@ public:
     template <typename T_other,size_t alignment_other>
     bool operator!=(AlignedAllocator<T_other,alignment_other> const&);
 };
+
+//
+template <typename T, size_t ALIGNMENT = 16>
+using AlignedMemory = std::vector<T, AlignedAllocator<T, ALIGNMENT>>;
+
+template <typename T,size_t alignment>
+inline
+AlignedAllocator<T,alignment>::AlignedAllocator(void)
+{
+}
+
+template <typename T,size_t alignment>
+template <typename T_other,size_t alignment_other>
+inline
+AlignedAllocator<T,alignment>::AlignedAllocator
+    (AlignedAllocator<T_other,alignment_other> const& other)
+{}
+
+template <typename T,size_t alignment>
+inline typename AlignedAllocator<T,alignment>::pointer
+AlignedAllocator<T,alignment>::allocate(unsigned long n)
+{
+    if (n > this->max_size())
+        throw std::bad_alloc();
+    size_t size = n * sizeof(T);
+    pointer p = nullptr;
+#if defined(_WIN32)
+    //TODO:WINDOWS
+#elif defined(linux)
+    //调用posix_memalign( )
+    //成功时会返回size字节的动态内存，并且这块内存的地址是alignment的倍数。
+    //参数alignment必须是2的幂，还是void指针的大小的倍数。
+    // 返回的内存块的地址放在了memptr里面，函数返回值是0.
+    //调用失败时，没有内存会被分配，memptr的值没有被定义
+    //---------------------
+    //作者：unix21
+    //来源：CSDN
+    //原文：https://blog.csdn.net/unix21/article/details/12893171
+    //版权声明：本文为博主原创文章，转载请附上博文链接！
+    if (::posix_memalign(&reinterpret_cast<void*&>(p),alignment,size))
+        throw std::bad_alloc();
+#endif
+    return p;
+}
+
+template <typename T,size_t alignment>
+inline void
+AlignedAllocator<T,alignment>::deallocate(util::AlignedAllocator<T, alignment>::pointer p, unsigned long n)
+{
+#ifdef _WIN32
+    //TODO:WINDOWS
+#else
+    ::free(p);
+#endif
+}
 
 
 
