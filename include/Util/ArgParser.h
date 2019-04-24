531ab8aa7b5a29effc280864c96bcc8cc54d4c4c
AnShuai666 <1028792866@qq.com>
2019-04-24 21:53:53 +0800

    添加参数声明

--- include/Util/ArgParser.h
+++ include/Util/ArgParser.h
@@ -10,9 +10,27 @@
 
 #include "Util/define.h"
 #include <iostream>
+#include <vector>
 UTIL_NAMESPACE_BEGIN
+
+struct ArgOption
+{
+    char short_option_name;
+    std::string long_option_name;
+    std::string description;
+    bool is_argument_needed;
+};
+
+struct ArgResult
+{
+    ArgOption const* arg_option;
+    std::string arg;
+
+    template <typename T>
+    T get_arg(void) const;
+};
 /********************************************************************
- *~~~~~~~~~~~~~~~~~~~~~文件系统相关函数声明~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *~~~~~~~~~~~~~~~~~~~~~~~~参数解析类声明~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  *******************************************************************/
 
 class Arguments
@@ -24,6 +42,15 @@ public:
 
 private:
    // std::size_t
+   bool auto_exit;
+   std::vector<ArgOption> arg_options;
+   std::string usage_str;
+   std::string description_str;
+   int help_text_indent;
+   int description_text_width;
+
+   std::vector<ArgResult> arg_results;
+   std::size_t current_result;
 };
 
  UTIL_NAMESPACE_END
