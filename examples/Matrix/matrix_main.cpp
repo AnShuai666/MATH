43b8a4340d403334e668dc0c16add4793275f796
AnShuai666 <1028792866@qq.com>
2019-04-24 21:20:26 +0800

    添加测试例程

--- examples/Matrix/matrix_main.cpp
+++ examples/Matrix/matrix_main.cpp
@@ -4,7 +4,14 @@
  * 姓名:
  * 邮箱:
 */
+#include "Matrix/matrix.hpp"
+//#include "Matrix/matrix_qr.hpp"
 int main()
 {
+    //Matrix<int,3,3> matrix1(3);
+   // Matrix<int,3,3> matrixQ;
+    //Matrix<int,3,3> matrixR;
+    //qr_decomposition(&matrix1,&matrixQ,&matrixR);
+
     return 0;
 }
--- examples/Util/logging_main.cpp
+++ examples/Util/logging_main.cpp
@@ -2,3 +2,17 @@
 // Created by yhl on 19-4-24.
 //
 
+#include <iostream>
+#include "Util/logging.h"
+
+int main()
+{
+    util::Logging logging(util::Logging::LOG_DEBUG);
+    logging.info()<<"info"<<std::endl;
+    logging.error()<<"error"<<std::endl;
+    logging.warning()<<"warning"<<std::endl;
+    logging.debug()<<"debug"<<std::endl;
+    logging.verbose()<<"verbose"<<std::endl;
+    std::cerr<<"cerr"<<std::endl;
+    return 0;
+};
