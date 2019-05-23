8a9d35a39830bc19e92fc1bce5a39ee96e558755
AnShuai666 <1028792866@qq.com>
2019-05-23 11:43:46 +0800

    修改bug

--- uninstallMATH.sh
+++ uninstallMATH.sh
@@ -6,3 +6,9 @@ rm -drf $INSTALL_DIR/include/MATH
 rm -drf $INSTALL_DIR/lib/MATH
 rm -drf $INSTALL_DIR/bin/MATH
 rm -drf $INSTALL_DIR/share/MATH
+
+#需要加条件 移除成功才显示
+echo "移除文件夹:$INSTALL_DIR/include/MATH"
+echo "移除文件夹:$INSTALL_DIR/lib/MATH"
+echo "移除文件夹:$INSTALL_DIR/bin/MATH"
+echo "移除文件夹:$INSTALL_DIR/share/MATH"
